f89bd71ada7946bed713126ad1b4ef0438d5dd14
ArmorDude <legend.ivanov@gmail.com>
2024-09-25 21:19:54 +0300

    Setup Express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('Just works');
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Just works');
+});
+
+app.listen(5000, () => console.log(`Server is listening on htpp://localhost:5000...`));
+
+
