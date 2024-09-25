8dfabea855ec889def0322aa115012453ded6c17
ArmorDude <legend.ivanov@gmail.com>
2024-09-25 21:50:39 +0300

    Render Home page

--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ import handlebars from 'express-handlebars';
 
 const app = express();
 
+// Handlebars setup
 app.engine('hbs', handlebars.engine({
     extname: 'hbs',
 }))
 app.set('view engine', 'hbs');
 app.set('views', './src/views');
 
+// Express setup
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('home');
 });
 
 app.listen(5000, () => console.log(`Server is listening on http://localhost:5000...`));
