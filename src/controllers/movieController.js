481e1079f3e15ac2cb0e18c6a27ce205d289436d
ArmorDude <legend.ivanov@gmail.com>
2024-09-28 12:12:04 +0300

    Fix details page with lean object querry

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -22,7 +22,7 @@ router.get('/search', async (req, res) => {
 
 router.get('/:movieId/details', async (req, res) => {
     const movieId = req.params.movieId;
-    const movie = await movieService.getOne(movieId);
+    const movie = await movieService.getOne(movieId).lean();
     movie.ratingView = getRatingViewData(movie.rating);
     res.render('movies/details', { movie });
 });
